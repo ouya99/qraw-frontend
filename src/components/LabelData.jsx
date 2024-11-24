2c9593b7ae7a9cdbf7e32492cd52ad7ac165a5f3
icyblob <dignity.rashes_0w@icloud.com>
2024-11-23 22:54:46 -0500

    Update LabelData to use LabelWithPopover inside

--- src/components/LabelData.jsx
+++ src/components/LabelData.jsx
@@ -1,8 +1,15 @@
-const LabelData = ({lbl, value}) => (
+import LabelWithPopover from "./qubic/ui/LabelWithPopover"
+
+const LabelData = ({lbl, value, description}) => (
   <div className="flex flex-col justify-center items-center">
-    <span className="text-gray-50 text-12">
+    {description ? (
+      <LabelWithPopover
+        label={lbl}
+        description={description}
+      />
+    ) : (<span className="text-gray-50 text-12">
       {lbl}
-    </span>
+    </span>)}
     <span className="text-white text-16">
       {value}
     </span>
