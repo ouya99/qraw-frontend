0d20668a943fded009c31b525328c4c4eed90e98
icyblob <dignity.rashes_0w@icloud.com>
2024-11-23 22:52:12 -0500

    Update LabelWithPopover UX
    We can close the tooltip anywhere outside the tooltip, not need to click on the icon again to close.

--- src/components/qubic/ui/LabelWithPopover.jsx
+++ src/components/qubic/ui/LabelWithPopover.jsx
@@ -1,35 +1,54 @@
-import React, { useState } from 'react';
-import circleInfo from '../../../assets/circle-info.svg';
+import React, { useState, useEffect, useRef } from 'react'
+import circleInfo from '../../../assets/circle-info.svg'
 
 const LabelWithPopover = ({ htmlFor, label, description }) => {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+  const popoverRef = useRef(null)
 
-  const togglePopover = () => {
-    setIsPopoverOpen(!isPopoverOpen);
-  };
+  const togglePopover = (e) => {
+    e.stopPropagation()
+    setIsPopoverOpen(!isPopoverOpen)
+  }
+
+  const handleClickOutside = (e) => {
+    if (popoverRef.current && !popoverRef.current.contains(e.target)) {
+      setIsPopoverOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isPopoverOpen) {
+      document.addEventListener('click', handleClickOutside)
+    } else {
+      document.removeEventListener('click', handleClickOutside)
+    }
+    // Cleanup event listener on unmount
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [isPopoverOpen])
 
   return (
-    <div className="relative inline-block">
+    <div className="relative flex items-center">
       <label htmlFor={htmlFor} className="text-white mb-2 flex items-center">
         {label}
+        <div className="ml-2 relative" ref={popoverRef}>
         <button
           type="button"
           onClick={togglePopover}
-          className="ml-2 text-gray-500 focus:outline-none"
+            className="focus:outline-none"
         >
-          <img src={circleInfo} alt="Info Icon" />
+          <img src={circleInfo} alt="Info Icon" className="w-4 h-4" />
         </button>
-      </label>
       {isPopoverOpen && (
-        <div className="absolute z-10 mt-2 p-2 w-56 bg-gray-700 text-white rounded-lg shadow-lg">
-          <div className="relative">
-            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-full w-3 h-3 bg-gray-700 rotate-45"></div>
+            <div className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-2 w-64 p-2 bg-gray-800 text-white text-sm rounded shadow-lg">
             {description}
           </div>
+          )}
         </div>
-      )}
+      </label>
     </div>
-  );
-};
+  )
+}
 
-export default LabelWithPopover;
+export default LabelWithPopover
