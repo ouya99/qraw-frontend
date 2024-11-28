0740a4e304d79e2c308495500722ab009c3e243f
icyblob <dignity.rashes_0w@icloud.com>
2024-11-28 18:39:49 -0500

    Misc decors for BetOverviewCard
    - Add status at the beginning of the bet desc
    -Add hot levels based on the pool's Qubics
    - Increase height for the bet desc area

--- src/components/BetOverviewCard.jsx
+++ src/components/BetOverviewCard.jsx
@@ -6,15 +6,49 @@ import LabelData from "./LabelData"
 import { sumArray } from "./qubic/util"
 import {formatDate} from "../components/qubic/util/commons"
 
-function BetOverviewCard({ data, onClick }) {
+function BetOverviewCard({ data, onClick, status='' }) {
+  let status_string = ''
+  switch (status) {
+    case 'active': {
+      status_string = '🟢'
+      break
+    }
+    case 'locked': {
+      status_string = '🔒'
+      break
+    }
+    case 'published': {
+      status_string = '🎉'
+      break
+    }
+    case 'waiting': {
+      status_string = '❓'
+      break
+    }
+    default: {
+      status_string = ''
+    }
+  }
+
+  let hot_level = ''
+  if (data.current_total_qus >= 1000000000) { // 1bil
+    hot_level = '💎'
+  } else if (data.current_total_qus >= 500000000) { // 500mil
+    hot_level = '🔥'
+  } else if (data.current_total_qus >= 100000000) { // 100mil
+    hot_level = '❤️'
+  }
+
   return (
     <Card
-      className="p-[15px] h-[280px] hover:border-primary-40 cursor-pointer"
+      className="p-[15px] h-[290px] hover:border-primary-40 cursor-pointer"
       onClick={onClick}
     >
       <div className="flex flex-col items-start justify-start justify-between gap-[8px] h-full">
         <div className="text-white text-20 line-clamp-3 overflow-hidden">
-          {data.full_description ? data.full_description : data.bet_desc}
+          {
+            `${status_string}${data.full_description ? data.full_description : data.bet_desc}`
+          }
         </div>
         <div className="grid grid-cols-2 justify-between items-center w-full">
           <LabelData lbl="Bet closes at"
@@ -31,6 +65,7 @@ function BetOverviewCard({ data, onClick }) {
           />
         </div>
         <div className="gap-[12px] flex justify-center items-center w-full">
+          ${hot_level}
           <img src={QubicCoin} alt="" />
           <span className="text-white text-18">
             {formatQubicAmount(data.current_total_qus)} QUBIC
