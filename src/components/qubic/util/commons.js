6d09b57a9a524097ddcc5c3235f5da8dd7f507cf
icyblob <dignity.rashes_0w@icloud.com>
2024-11-29 00:36:02 -0500

    Hide bet 76

--- src/components/qubic/util/commons.js
+++ src/components/qubic/util/commons.js
@@ -11,7 +11,7 @@ export const HEADERS = {
 export const MIN_BALANCE_THRESHOLD = 100
 export const QTRY_CONTRACT_INDEX = 2
 export const LOG_DEBUG = false
-export const excludedBetIds = [31, 34, 58]
+export const excludedBetIds = [31, 34, 58, 76]
 export const makeJsonData = (contractIndex, inputType, inputSize, requestData) => {
   return {
     contractIndex: contractIndex,
